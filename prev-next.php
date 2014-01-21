383eff28fa9562c9abab66418fb374b2265c553f
cpresler <hello@christypresler.com>
2014-01-21 16:59:13 -0600

    Fix spacing issue

--- prev-next.php
+++ prev-next.php
@@ -8,22 +8,22 @@
 <body>
 <div class="post-nav">
 <?php
-			if (is_single()) { ?>   <!-- tests for single page-->
-				<div class="prev">
-					<?php previous_post('%', 'prev', 'no'); ?>   <!-- ('display with link%', 'text displayed', "title y/n") -->
-				</div>
-				<div class="next">
-					<?php next_post('%', 'next', 'no'); ?>
-				</div>		
-		<?php }	?>
-		<?php if (is_home() || is_front_page() || is_archive() || is_search() || is_paged() && have_posts()) { ?>  <!-- tests if for post list page, and posts-->
-				<div class="new">
-					<?php echo get_previous_posts_link('newer posts'); ?>    <!-- ('test displayed', max # pages) -->
-				</div>
-				<div class="old">
-					<?php echo get_next_posts_link('older posts'); ?>
-				</div>		
-		<?php } ?>
+	if (is_single()) { ?>   <!-- tests for single page-->
+		<div class="prev">
+			<?php previous_post('%', 'prev', 'no'); ?>   <!-- ('display with link%', 'text displayed', "title y/n") -->
+		</div>
+		<div class="next">
+			<?php next_post('%', 'next', 'no'); ?>
+		</div>		
+	<?php }	?>
+	<?php if (is_home() || is_front_page() || is_archive() || is_search() || is_paged() && have_posts()) { ?>  <!-- tests if for post list page, and posts-->
+		<div class="new">
+			<?php echo get_previous_posts_link('newer posts'); ?>    <!-- ('test displayed', max # pages) -->
+		</div>
+		<div class="old">
+			<?php echo get_next_posts_link('older posts'); ?>
+		</div>		
+	<?php } ?>
 </div>
 
 </body>
