67e6be4f0eea1c870e3ee69ac6223ce8e78c2c37
cpresler <hello@christypresler.com>
2014-01-23 20:01:45 -0600

    Links not directing properly, fixed with a loop
    The Prev and Next links weren't directing properly on the live server.
    Encompassed them in a loop and updated the functions.

--- prev-next.php
+++ prev-next.php
@@ -8,22 +8,27 @@
 <body>
 <div class="post-nav">
 <?php
-	if (is_single()) { ?>   <!-- tests for single page-->
-		<div class="prev">
-			<?php previous_post('%', 'prev', 'no'); ?>   <!-- ('display with link%', 'text displayed', "title y/n") -->
-		</div>
-		<div class="next">
-			<?php next_post('%', 'next', 'no'); ?>
-		</div>		
-	<?php }	?>
-	<?php if (is_home() || is_front_page() || is_archive() || is_search() || is_paged() && have_posts()) { ?>  <!-- tests if for post list page, and posts-->
+	if (is_single()) { 
+		if (have_posts()) { 
+			while (have_posts()) {
+			 the_post(); ?>
+			 	<div class="prev">
+			 		<?php previous_post_link('%link', 'prev'); ?>
+					</div>
+				<div class="next">
+					<?php next_post_link('%link', 'next'); ?>
+				</div>
+		<?php }
+		}
+	 } else { ?>
 		<div class="new">
-			<?php echo get_previous_posts_link('newer posts'); ?>    <!-- ('test displayed', max # pages) -->
+			<?php echo get_previous_posts_link('newer posts'); ?>    <!-- ('text displayed', max # pages) -->
 		</div>
 		<div class="old">
 			<?php echo get_next_posts_link('older posts'); ?>
 		</div>		
-	<?php } ?>
+<?php } ?>
+
 </div>
 
 </body>
